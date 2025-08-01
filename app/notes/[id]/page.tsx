378c00d5350b20749180bcefd4e0b291914a07f2
Misha Busko <Misha.1996@ukr.net>
2025-08-01 23:09:02 +0300

    First commit

---
+++ app/notes/[id]/page.tsx
@@ -0,0 +1,28 @@
+import {
+  QueryClient,
+  HydrationBoundary,
+  dehydrate,
+} from "@tanstack/react-query";
+import { fetchNoteById } from "@/lib/api";
+import NoteDetailsClient from "@/app/notes/[id]/NoteDetails.client";
+
+type Props = {
+  params: { id: string };
+};
+
+export default async function NoteDetailsPage({ params }: Props) {
+  const noteId = params.id;
+
+  const queryClient = new QueryClient();
+
+  await queryClient.prefetchQuery({
+    queryKey: ["notes", noteId],
+    queryFn: () => fetchNoteById(noteId),
+  });
+
+  return (
+    <HydrationBoundary state={dehydrate(queryClient)}>
+      <NoteDetailsClient />
+    </HydrationBoundary>
+  );
+}
